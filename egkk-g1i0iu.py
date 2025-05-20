4e3f624f5e946df26ff4834196bc00566f7cf6f4
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2025-05-20 23:05:59 +0300

    Add EGKK gate 21.

--- egkk-g1i0iu.py
+++ egkk-g1i0iu.py
@@ -2,9 +2,19 @@
 
 msfs_mode = 1
 
+@AlternativeStopPositions
+def _21(aircraftData):
+
+  table = {
+    "A320": 0
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
 parkings = {
   GATE: {
     None: (CustomizedName("Default|Gate #§"), ),
+    21: (CustomizedName("Pier 2|Gate #"), _21),
     33: (CustomizedName("Pier 3|Gate # (Heavy)"), ),
   },
 }

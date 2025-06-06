fa5be3ebeb9033db54e2c6f0a0827232550b26a7
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2025-06-06 23:46:20 +0300

    Add LEMD T20, 155.

--- lemd-btgqwj.py
+++ lemd-btgqwj.py
@@ -3,6 +3,15 @@
 msfs_mode = 1
 
 @AlternativeStopPositions
+def _155(aircraftData):
+
+  table = {
+    "B77L": 0
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
+@AlternativeStopPositions
 def _346(aircraftData):
 
   table = {
@@ -20,9 +29,22 @@ def t13(aircraftData):
 
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
+@AlternativeStopPositions
+def t20(aircraftData):
+
+  table = {
+    "B772": 0
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
 parkings = {
+  GATE: {
+    155: (CustomizedName("Ramp R-5|Stand #"), _155),
+  },
   GATE_T: {
     None: (CustomizedName("Default|Gate T#"), ),
-    13: (CustomizedName("Terminal 1|Gate T#"), t13),
+    13: (CustomizedName("Ramp R-3|Gate T#"), t13),
+    20: (CustomizedName("Ramp R-2|Gate T#"), t20),
   },
 }

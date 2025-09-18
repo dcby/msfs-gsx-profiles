be8152405bfffc22d1854e9a19cd1d439b6c0486
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2025-09-18 21:58:32 +0300

    Update EPKK.

--- epkk-jb8fos.py
+++ epkk-jb8fos.py
@@ -11,9 +11,19 @@ def _12(aircraftData):
 
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
+@AlternativeStopPositions
+def _15(aircraftData):
+
+  table = {
+    "A320": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
 parkings = {
   GATE: {
     None: (CustomizedName("Default|Gate #"), ),
     12: (CustomizedName("Terminal|Gate #"), _12),
+    15: (CustomizedName("Terminal|Gate #"), _15),
   },
 }

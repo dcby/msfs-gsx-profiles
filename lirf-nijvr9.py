0ee3a2c48d1f4ebdf9ccd1cb75c65ba7e229b92f
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2025-09-24 22:14:20 +0300

    Enhance LIRF.

--- lirf-nijvr9.py
+++ lirf-nijvr9.py
@@ -3,6 +3,15 @@
 msfs_mode = 1
 
 @AlternativeStopPositions
+def _312(aircraftData):
+
+  table = {
+    "A320": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
+@AlternativeStopPositions
 def _402(aircraftData):
 
   table = {
@@ -29,10 +38,21 @@ def _409(aircraftData):
 
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
+@AlternativeStopPositions
+def _505(aircraftData):
+
+  table = {
+    "A320": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
 parkings = {
   GATE: {
+    312: (CustomizedName("Terminal 1|Gate #"), _312),
     402: (CustomizedName("Terminal 1|Gate #"), _402),
     407: (CustomizedName("Terminal 1|Gate #"), _407),
     409: (CustomizedName("Terminal 1|Gate #"), _409),
+    505: (CustomizedName("Terminal 1|Gate #"), _505),
   },
 }

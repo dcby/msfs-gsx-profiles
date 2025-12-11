3dad44e3dbcbb26e386d38328a0d64f57dea21f7
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2025-12-11 20:57:26 +0300

    Enhance KATL.

--- katl-musaom.py
+++ katl-musaom.py
@@ -3,6 +3,15 @@
 msfs_mode = 1
 
 @AlternativeStopPositions
+def b26(aircraftData):
+
+  table = {
+    "A321": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
+@AlternativeStopPositions
 def e10(aircraftData):
 
   table = {
@@ -12,6 +21,9 @@ def e10(aircraftData):
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
 parkings = {
+  GATE_B: {
+    26: (CustomizedName("Concourse B|Gate B#"), b26),
+  },
   GATE_E: {
     10: (CustomizedName("Concourse E|Gate E#"), e10),
   },

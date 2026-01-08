09cfb9b0990e6243817024fe7648b9ca5c6b93bd
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2026-01-08 22:04:04 +0300

    Update profiles.

--- ksea-kle_ki.py
+++ ksea-kle_ki.py
@@ -3,6 +3,15 @@
 msfs_mode = 1
 
 @AlternativeStopPositions
+def b8(aircraftData):
+
+  table = {
+    "B738": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
+@AlternativeStopPositions
 def s1(aircraftData):
 
   table = {
@@ -12,7 +21,10 @@ def s1(aircraftData):
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
 parkings = {
+  GATE_B: {
+    8: (CustomizedName("(c) Concourse B|Stand B#"), b8),
+  },
   S_PARKING: {
-    1: (CustomizedName("Cargo Area 3|Stand #"), s1),
+    1: (CustomizedName("(c) Cargo Area 3|Stand #"), s1),
   },
 }

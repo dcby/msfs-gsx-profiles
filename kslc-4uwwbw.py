bae550cfdcdfa60c9901a2e701fa619f1dbe22d1
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2026-02-03 23:07:20 +0300

    Update KSLC.

--- kslc-4uwwbw.py
+++ kslc-4uwwbw.py
@@ -3,17 +3,17 @@
 msfs_mode = 1
 
 @AlternativeStopPositions
-def gate_b20(aircraftData):
+def b20(aircraftData):
 
   table = {
     # "A320": -2.45,
-    "B738": -2.45
+    "B738": 0,
   }
 
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
 parkings = {
   GATE_B: {
-    20: (CustomizedName("Concourse B|Gate B#"), gate_b20),
+    20: (CustomizedName("(c) Concourse B|Gate B#"), b20),
   },
 }

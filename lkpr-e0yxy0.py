7715731372b02da337d26cea7c23c6a920c8b8e6
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2026-02-09 22:07:43 +0300

    Update LKPR.

--- lkpr-e0yxy0.py
+++ lkpr-e0yxy0.py
@@ -11,8 +11,28 @@ def _16(aircraftData):
 
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
+@AlternativeStopPositions
+def _23(aircraftData):
+
+  table = {
+    "B738": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
+@AlternativeStopPositions
+def _26(aircraftData):
+
+  table = {
+    "B738": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
 parkings = {
   GATE: {
-    16: (CustomizedName("Pier B|Gate #"), _16),
+    16: (CustomizedName("(c) Pier B|Gate #"), _16),
+    23: (CustomizedName("(c) Pier C|Gate #"), _23),
+    26: (CustomizedName("(c) Terminal 2|Gate #"), _26),
   },
 }

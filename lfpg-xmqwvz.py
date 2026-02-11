187671ceeb9941c61de54ff94d00f6e4a5929b2a
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2026-02-11 22:00:42 +0300

    Update LFPG.

--- lfpg-xmqwvz.py
+++ lfpg-xmqwvz.py
@@ -3,6 +3,15 @@
 msfs_mode = 1
 
 @AlternativeStopPositions
+def d16(aircraftData):
+
+  table = {
+    "B738": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
+@AlternativeStopPositions
 def x1(aircraftData):
 
   table = {
@@ -12,7 +21,10 @@ def x1(aircraftData):
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
 parkings = {
+  GATE_D: {
+    16: (CustomizedName("(c) Terminal 2|Gate D#"), d16),
+  },
   GATE_X: {
-    1: (CustomizedName("Terminal 1|Gate X#"), x1),
+    1: (CustomizedName("(c) Terminal 1|Gate X#"), x1),
   },
 }

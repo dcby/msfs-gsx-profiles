13fed4b223a3e70f51ab0a70c35260a46afa7710
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2026-02-23 21:45:03 +0300

    Update EDDB.

--- eddb-4n29rl.py
+++ eddb-4n29rl.py
@@ -3,6 +3,15 @@
 msfs_mode = 1
 
 @AlternativeStopPositions
+def a7(aircraftData):
+
+  table = {
+    "B738": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
+@AlternativeStopPositions
 def c5(aircraftData):
 
   table = {
@@ -12,7 +21,10 @@ def c5(aircraftData):
   return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
 
 parkings = {
+  GATE_A: {
+    7: (CustomizedName("(c) Apron A|Stand A#"), a7),
+  },
   GATE_C: {
-    5: (CustomizedName("Apron C|Gate C#"), c5),
+    5: (CustomizedName("(c) Apron C|Gate C#"), c5),
   },
 }

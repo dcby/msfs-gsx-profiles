d6c6436c5f56f234784872697fe8136fa97e3af6
Fiodar Buyakou <10463160+dcby@users.noreply.github.com>
2026-02-25 12:13:31 +0300

    Update LIMF.

--- limf-9yx9ww.py
+++ limf-9yx9ww.py
@@ -2,8 +2,18 @@
 
 msfs_mode = 1
 
+@AlternativeStopPositions
+def _107(aircraftData):
+
+  table = {
+    "B738": 0,
+  }
+
+  return Distance.fromMeters(table.get(aircraftData.icaoTypeDesignator, 0))
+
 parkings = {
   GATE: {
-    111: (CustomizedName("Main Apron / Middle Area|Gate #"), ),
+    107: (CustomizedName("(c) Main Apron / North Area|Stand #"), _107),
+    111: (CustomizedName("(c) Main Apron / Middle Area|Gate #"), ),
   },
 }
